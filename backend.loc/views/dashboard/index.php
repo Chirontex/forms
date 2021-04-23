e2a693995c2d77ed24e8a460a69fe9c4c532bfeb
Dmitry Shumilin <infernozzz@yandex.ru>
2021-04-23 22:21:45 +0300

    0.0.3: update index view

--- backend.loc/views/dashboard/index.php
+++ backend.loc/views/dashboard/index.php
@@ -1,21 +1,36 @@
 <?php
 
-$this->title = 'Test';
+use yii\helpers\Html;
+
+$this->title = 'Пожалуйста, авторизуйтесь';
 
 ?>
-<link rel="stylesheet" href="css/signin.css">
+<link rel="stylesheet" href="css/signin.css?v=0.0.2">
 <main class="form-signin text-center">
-    <form>
+<?php
+
+if (isset($message)) {
+
+?>
+    <div class="alert alert-<?= Html::encode($message['type']) ?>" role="alert">
+        <?= Html::encode($message['text']) ?>
+    </div>
+<?php
+
+}
+
+?>
+    <form action="" method="post">
         <h1 class="h3 mb-3 fw-normal">Пожалуйста, авторизуйтесь</h1>
-        <div class="mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="e-mail">
+        <div>
+            <input type="email" class="form-control" name="input" placeholder="e-mail" required="true">
         </div>
         <div class="mb-3">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="пароль">
+            <input type="password" class="form-control" name="password" placeholder="пароль" required="true">
         </div>
         <div class="checkbox mb-3">
             <label>
-                <input type="checkbox" value="remember-me"> Запомнить меня
+                <input type="checkbox" name="remember" value="true"> Запомнить меня
             </label>
         </div>
         <button class="w-100 btn btn-lg btn-primary" type="submit">Войти</button>
